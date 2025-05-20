ce69ee3e7c7928a2331654c94db59a772f654f8e
Victor3294 <vblum26@gmail.com>
2025-05-20 10:53:59 -0300

    estilizando a pagina nossos videos

--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -5,8 +5,8 @@ import VideoGallery from '@/components/VideoGallery';
 
 export default function Home() {
   return (
-    <div className="max-w-[1200px] m-auto overflow-x-hidden">
-      <div className="flex-grow">
+    <div className="m-auto overflow-x-hidden bg-gradient-to-b from-primary via-black/80 to-primary min-h-screen">
+      <div className="flex-grow pb-20">
         <Header />
         <div className="py-20 h-32 mb-20">
           <p className="text-3xl font-bold text-center text-primary-foreground pb-20">
