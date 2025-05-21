b828199f555cd7a30c2b11bcabda207995671ace
NicolasArthurDev <Nicolasarthur1806@gmail.com>
2025-05-21 19:41:43 -0300

    fix: corrigindo o nome da pagina artigos

--- src/app/artigos/page.tsx
+++ src/app/artigos/page.tsx
@@ -48,7 +48,7 @@ export default function Galeria() {
       <main className="flex-grow bg-white py-10 px-4">
         <div className="max-w-6xl mx-auto space-y-12">
           <h1 className="text-3xl font-bold text-center text-primary mb-8">
-            Acervo do Museu
+            Artigos do Museu
           </h1>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
