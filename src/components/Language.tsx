0201e09c64448404efa5c29200f864836c890b18
leticiavieira0 <leticiapitica03@gmail.com>
2026-05-29 20:22:46 -0300

    feat: Tradução e estilização

--- src/components/Language.tsx
+++ src/components/Language.tsx
@@ -1,7 +1,8 @@
 "use client";
+import "../app/globals.css";
+import { useEffect, useState } from "react";
 
-import { useEffect } from "react";
-import { color } from "three/src/nodes/TSL.js";
+
 
 declare global {
   interface Window {
@@ -11,6 +12,8 @@ declare global {
 }
 
 export default function Translate() {
+  const [showModal, setShowModal] = useState(true)
+
   useEffect(() => {
     // Only add the script if it doesn't exist yet
     if (!document.getElementById("google-translate-script")) {
@@ -47,14 +50,33 @@ export default function Translate() {
 
   return (
     <div>
-      <div id="google_translate_element" style={{ display: "none" }} />
+      <div id="google_translate_element" />
+
+      {showModal && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80 transition-opacity duration-200">
+          <div className="bg-black rounded-lg p-4 sm:px-3 sm:py-1 w-full max-w-lg sm:max-w-md md:max-w-2xl lg:max-w-5xl relative transform transition-all duration-200 scale-95 opacity-0 animate-modalIn mx-2">
+            <p className="text-center font-worksans">Conheça o nosso Museu em outras línguas!</p>
+            <button
+              type="button"
+              className="absolute sm:top-0 sm:right-2 sm:text-2xl top-0 right-2 text-2xl"
+              onClick={() => setShowModal(false)}
+            >
+              &times;
+            </button>
+
+              <div className="outras_linhas">
+                <div className="conteiner_outras_linhas">
+                  <button onClick={() => {changeLanguage("en"), setShowModal(false)}} className="button_lenguage">English</button>
+                  <button onClick={() => {changeLanguage("es"), setShowModal(false)}} className="button_lenguage">Español</button>
+                  <button onClick={() => {changeLanguage("pt"), setShowModal(false)}} className="button_lenguage">Português</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
 
 
-    <div>
-      <button onClick={() => changeLanguage("en")}>English</button>
-      <button onClick={() => changeLanguage("es")}>Español</button>
-      <button onClick={() => changeLanguage("pt")}>Português</button>
-    </div>
+    
 
     </div>
   );
